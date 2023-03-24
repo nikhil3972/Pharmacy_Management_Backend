29487d6b77c136264a428268e5aef925d8822030
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-03-24 10:57:37 +0530

    Initial Setup

---
+++ src/test/java/com/manager/demo/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
