66523dbeaa29cb77eeb4be4238a16a3eda96310f
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-03-25 23:05:51 +0530

    Disease_Type Table Done

---
+++ src/main/java/com/manager/demo/entity/Disease_Type.java
@@ -0,0 +1,76 @@
+package com.manager.demo.entity;
+
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Disease_Type")
+public class Disease_Type {
+	
+	@Id
+	int id;
+	String type;
+	
+	@OneToMany(targetEntity = Medicine.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "disease_type_fk", referencedColumnName = "id")
+	List<Medicine> medicine;
+	
+	String created_by;
+	String modified_by;
+	Date created_ts;
+	Date modified_ts;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public List<Medicine> getMedicine() {
+		return medicine;
+	}
+	public void setMedicine(List<Medicine> medicine) {
+		this.medicine = medicine;
+	}
+	public String getCreated_by() {
+		return created_by;
+	}
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+	public String getModified_by() {
+		return modified_by;
+	}
+	public void setModified_by(String modified_by) {
+		this.modified_by = modified_by;
+	}
+	public Date getCreated_ts() {
+		return created_ts;
+	}
+	public void setCreated_ts(Date created_ts) {
+		this.created_ts = created_ts;
+	}
+	public Date getModified_ts() {
+		return modified_ts;
+	}
+	public void setModified_ts(Date modified_ts) {
+		this.modified_ts = modified_ts;
+	}
+	@Override
+	public String toString() {
+		return "Disease_Type [id=" + id + ", type=" + type + ", medicine=" + medicine + ", created_by=" + created_by
+				+ ", modified_by=" + modified_by + ", created_ts=" + created_ts + ", modified_ts=" + modified_ts + "]";
+	}	
+}
---
+++ src/main/java/com/manager/demo/repository/Disease_TypeRepository.java
@@ -0,0 +1,9 @@
+package com.manager.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manager.demo.entity.Disease_Type;
+
+public interface Disease_TypeRepository extends JpaRepository<Disease_Type, Integer> {
+
+}
