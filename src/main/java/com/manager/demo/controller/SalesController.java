199d519a7be5532e635686e6c5e55c841d43439f
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-03-27 19:33:41 +0530

    Sales Table Done

---
+++ src/main/java/com/manager/demo/controller/SalesController.java
@@ -0,0 +1,67 @@
+package com.manager.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manager.demo.entity.Sales;
+import com.manager.demo.repository.SalesRepository;
+
+@RestController
+public class SalesController {
+
+	@Autowired
+	SalesRepository saleRepo;
+	
+	@GetMapping(path="/getAllSales")
+	public List<Sales> getAllSales() {
+		List<Sales> sale = saleRepo.findAll();
+		System.out.println("Get list of all Sales successfully");
+		return sale;
+	}
+	
+	@PostMapping(path="/insertSales")
+	public String insertSales(@RequestBody Sales obj) {
+		System.out.println("Received data : " + obj);
+		saleRepo.save(obj);
+		return "Record Inserted Successfully";
+	}
+	
+	@PutMapping(path="/updateSales")
+	public String updateSales(@RequestBody Sales obj) {
+		Optional<Sales> sale = saleRepo.findById(obj.getId());
+		
+		if(sale.isPresent()) {
+			Sales saleUpd = sale.get();
+			saleUpd.setDate(obj.getDate());
+			saleUpd.setCustomer(obj.getCustomer());
+			saleUpd.setTotal_cost(obj.getTotal_cost());
+			saleUpd.setCreated_by(obj.getCreated_by());
+			saleUpd.setModified_by(obj.getModified_by());
+			saleUpd.setCreated_ts(obj.getCreated_ts());
+			saleUpd.setModified_ts(obj.getModified_ts());
+			System.out.println("Received Data in PutMapping :" + obj);
+			saleRepo.save(obj);
+			return "Record Updated Successfully";
+		}
+		else {
+			return "Unable to update the record";
+		}
+	}
+	
+	@DeleteMapping (path="/deleteSales/{id}")
+	public String deleteSales(@PathVariable int id) {
+		System.out.println("Sales record deleted. Given id : " + id);
+		saleRepo.deleteById(id);
+		return "Record Deleted Successfully";
+	}
+	
+}
---
+++ src/main/java/com/manager/demo/entity/Sales.java
@@ -0,0 +1,117 @@
+package com.manager.demo.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Sales")
+public class Sales {
+
+	@Id
+	int id;
+	String date;
+	
+	@OneToMany(targetEntity = Customer.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "sales_fk", referencedColumnName = "id")
+	List<Customer> customer;
+	
+	BigDecimal total_cost;
+	String created_by;
+	String modified_by;
+	Date created_ts;
+	Date modified_ts;
+	
+	Sales(){}
+
+	public Sales(int id, String date, List<Customer> customer, BigDecimal total_cost, String created_by,
+			String modified_by, Date created_ts, Date modified_ts) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.customer = customer;
+		this.total_cost = total_cost;
+		this.created_by = created_by;
+		this.modified_by = modified_by;
+		this.created_ts = created_ts;
+		this.modified_ts = modified_ts;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public List<Customer> getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(List<Customer> customer) {
+		this.customer = customer;
+	}
+
+	public BigDecimal getTotal_cost() {
+		return total_cost;
+	}
+
+	public void setTotal_cost(BigDecimal total_cost) {
+		this.total_cost = total_cost;
+	}
+
+	public String getCreated_by() {
+		return created_by;
+	}
+
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+
+	public String getModified_by() {
+		return modified_by;
+	}
+
+	public void setModified_by(String modified_by) {
+		this.modified_by = modified_by;
+	}
+
+	public Date getCreated_ts() {
+		return created_ts;
+	}
+
+	public void setCreated_ts(Date created_ts) {
+		this.created_ts = created_ts;
+	}
+
+	public Date getModified_ts() {
+		return modified_ts;
+	}
+
+	public void setModified_ts(Date modified_ts) {
+		this.modified_ts = modified_ts;
+	}
+
+	@Override
+	public String toString() {
+		return "Sales [id=" + id + ", date=" + date + ", customer=" + customer + ", total_cost=" + total_cost
+				+ ", created_by=" + created_by + ", modified_by=" + modified_by + ", created_ts=" + created_ts
+				+ ", modified_ts=" + modified_ts + "]";
+	}
+}
