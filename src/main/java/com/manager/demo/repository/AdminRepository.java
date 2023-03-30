9ce28a9ca901feacbdd8da3145681e4bf0c85ee8
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-03-30 10:28:30 +0530

    Login with security

--- src/main/java/com/manager/demo/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manager.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.manager.demo.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-}
