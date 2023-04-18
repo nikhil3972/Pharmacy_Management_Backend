5ccc651010cec4ae92542720e260a2d3bbf54f47
Prasad <prasadsp2001@gmail.com>
2023-04-18 08:40:00 +0530

    Added DockerFile

--- src/main/java/com/manager/demo/PharmacyManagementApplication.java
+++ src/main/java/com/manager/demo/PharmacyManagementApplication.java
@@ -2,9 +2,17 @@ package com.manager.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PharmacyManagementApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Springboot Backend Docker";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyManagementApplication.class, args);
